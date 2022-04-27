320bedb151311319ac79434b3f7c1c52af991e36
仁斎 <91141283+jinssai@users.noreply.github.com>
2022-04-28 01:01:44 +0900

    Add files via upload

---
+++ assets/js/submit.js
@@ -0,0 +1,59 @@
+function submit() {
+  let URL = document.getElementById('inputURL').value;
+　　const textinput = document.getElementById('inputURL');
+  const iframe = document.getElementById('iframe');
+  const error_message = document.getElementById('error_message');
+  let search_result = URL.indexOf('youtube.com/watch?v=');
+  let msearch_result = URL.indexOf('youtu.be/');
+  let amp_check = URL.indexOf('&');
+　　let length = URL.length;
+    if( search_result !== -1 ) {
+      if( !error_message.hasAttribute('class') ) {
+        error_message.setAttribute('class','hide');
+      }
+      hide('output_placeholder');
+      if( amp_check !== -1 ) {
+        let v = URL.substring(search_result+20,amp_check);
+        let newURL = 'https://www.youtube.com/embed/' + v + '?start=0&rel=0&feature=youtu.be';
+        iframe.setAttribute('src',newURL);
+      } else {
+        let v = URL.substring(search_result+20,length);
+        let newURL = 'https://www.youtube.com/embed/' + v + '?start=0&rel=0&feature=youtu.be';
+        iframe.setAttribute('src',newURL);
+      }
+    } else if( msearch_result !== -1 ) {
+      if( !error_message.hasAttribute('class') ) {
+        error_message.setAttribute('class','hide');
+      }
+      hide('output_placeholder');
+      if( amp_check !== -1) {
+      let v = URL.substring(msearch_result+9,amp_check);
+      let newURL = 'https://www.youtube.com/embed/' + v + '?start=0&rel=0&feature=youtu.be';
+      iframe.setAttribute('src',newURL);
+      } else {
+      let v = URL.substring(msearch_result+9,length);
+      let newURL = 'https://www.youtube.com/embed/' + v + '?start=0&rel=0&feature=youtu.be';
+      iframe.setAttribute('src',newURL);
+      }
+    } else {
+      if( iframe.hasAttribute('src') ) {
+        iframe.removeAttribute('src');
+      }
+      hide('output_placeholder');
+      show('error_message');
+    }
+}
+function hide(element_name) {
+  let element = document.getElementById(element_name);
+  element.setAttribute('class','hide');
+}
+function show(element_name) {
+  let element = document.getElementById(element_name);
+  element.removeAttribute('class');
+}
+document.getElementById('inputURL').onkeypress = (e) => {
+  const key = e.keyCode || e.charCode || 0;
+  if (key == 13) {
+    submit();
+  }
+}
