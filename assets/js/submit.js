b63e306506e897542ed0d6418ffd5130301a3d76
仁斎 <91141283+jinssai@users.noreply.github.com>
2022-05-04 16:33:23 +0900

    Update submit.js

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -1,48 +1,41 @@
 function submit() {
-  let URL = document.getElementById('inputURL').value;
-　　const textinput = document.getElementById('inputURL');
+　const textinput = document.getElementById('inputURL');
   const iframe = document.getElementById('iframe');
   const error_message = document.getElementById('error_message');
+  let URL = document.getElementById('inputURL').value;
   let search_result = URL.indexOf('youtube.com/watch?v=');
   let msearch_result = URL.indexOf('youtu.be/');
   let amp = URL.indexOf('&');
   let quest = URL.indexOf('?');
-　　let length = URL.length;
-    if( search_result !== -1 ) {
-      if( !error_message.hasAttribute('class') ) {
-        error_message.setAttribute('class','hide');
-      }
-      hide('output_placeholder');
-      if( amp !== -1 ) {
-        let v = URL.substring(search_result+20,amp);
-        let newURL = 'https://www.youtube.com/embed/' + v + '?start=0&rel=0&feature=youtu.be';
-        iframe.setAttribute('src',newURL);
-      } else {
-        let v = URL.substring(search_result+20,length);
-        let newURL = 'https://www.youtube.com/embed/' + v + '?start=0&rel=0&feature=youtu.be';
-        iframe.setAttribute('src',newURL);
-      }
-    } else if( msearch_result !== -1 ) {
-      if( !error_message.hasAttribute('class') ) {
-        error_message.setAttribute('class','hide');
-      }
-      hide('output_placeholder');
-      if( quest !== -1) {
-      let v = URL.substring(msearch_result+9,quest);
-      let newURL = 'https://www.youtube.com/embed/' + v + '?start=0&rel=0&feature=youtu.be';
-      iframe.setAttribute('src',newURL);
-      } else {
-      let v = URL.substring(msearch_result+9,length);
-      let newURL = 'https://www.youtube.com/embed/' + v + '?start=0&rel=0&feature=youtu.be';
-      iframe.setAttribute('src',newURL);
-      }
-    } else {
-      if( iframe.hasAttribute('src') ) {
-        iframe.removeAttribute('src');
-      }
-      hide('output_placeholder');
-      show('error_message');
+　let length = URL.length;
+  if( search_result !== -1 ) {
+    if( !error_message.hasAttribute('class') ) {
+      hide('error_message');
     }
+    hide('output_placeholder');
+    if( amp !== -1 ) {
+      ytdisplay(search_result+20,amp);
+    } else {
+      ytdisplay(search_result+20,length);
+    }
+  } else if( msearch_result !== -1 ) {
+    if( !error_message.hasAttribute('class') ) {
+      hide('error_message');
+    }
+    hide('output_placeholder');
+    if( quest !== -1) {
+      ytdisplay(msearch_result+9,quest);
+    } else {
+      ytdisplay(msearch_result+9,length);
+    }
+  } else {
+    if( iframe.hasAttribute('src') ) {
+      iframe.removeAttribute('src');
+    }
+    hide('output_placeholder');
+    show('error_message');
+    textinput.blur();
+  }
 }
 function hide(element_name) {
   let element = document.getElementById(element_name);
@@ -51,6 +44,14 @@ function hide(element_name) {
 function show(element_name) {
   let element = document.getElementById(element_name);
   element.removeAttribute('class');
+}
+function ytdisplay(start,end) {
+　　const textinput = document.getElementById('inputURL');
+  let URL = document.getElementById('inputURL').value;
+  let v = URL.substring(start,end);
+  let newURL = 'https://www.youtube.com/embed/' + v + '?start=0&rel=0&feature=youtu.be';
+  iframe.setAttribute('src',newURL);
+  textinput.blur();
 }
 document.getElementById('inputURL').onkeypress = (e) => {
   const key = e.keyCode || e.charCode || 0;
